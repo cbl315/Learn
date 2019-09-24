e6ec8b59c9471dd7c709b9cf45e28b0a54ab8d8d
woshicai <583605554@qq.com>
2019-09-24 23:28:10 +0800

    ADD: TGPL 7.1

--- Golang/src/TGPL/ch4/tree.go
+++ Golang/src/TGPL/ch4/tree.go
@@ -1,8 +1,29 @@
 package main
 
+import "fmt"
+
 type tree struct {
 	value       int
 	left, right *tree
+}
+
+// 打印tree的值序列
+// 思路: 使用 slice 存储二叉树的值 根据列表的长度 打印值序列
+func (t *tree) String() string {
+	var biT []int
+	biT = appendValues(biT, t)
+	return fmt.Sprintf("%v", biT)
+}
+
+func InitTree(values []int) *tree {
+	var root *tree
+	for _, v := range values {
+		root = add(root, v)
+	}
+	//b := values[:0]
+	//fmt.Printf("%v", b)
+	appendValues(values[:0], root)
+	return root
 }
 
 func Sort(values []int) {
