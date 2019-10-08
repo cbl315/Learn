8dc815a65f2380365a7bc47207e23f0b0bfbc7f2
woshicai <583605554@qq.com>
2019-10-09 07:25:23 +0800

    TGPL: 7.6 study part

--- Golang/src/TGPL/ch7/ch7_main.go
+++ Golang/src/TGPL/ch7/ch7_main.go
@@ -1,7 +1,29 @@
+/*
+interface的零值： 类型和值的部分都是nil
+
+*/
 package main
+
+import (
+	"bytes"
+	"io"
+	"os"
+)
+
+func interfaceValue() {
+	var w io.Writer
+	//w.Write(([]byte)("Hello"))  // panic: nil pointer dereference
+	w = os.Stdout // equals to io.Writer(os.Stdout)
+	w.Write(([]byte)("Hello"))
+	w = new(bytes.Buffer)
+	w = nil
+}
 
 func main() {
 	//testByteCounter()
 	//testNewReader("test")
-	sleep()
+	//sleep()
+	//interfaceValue()
+	printTracks(tracks)
+	sortTracks()
 }
