43d7d1e89e06700fd1143a1c6f772a6d8eb1511d
i535989 <charles.cat@sap.com>
2020-09-27 17:01:59 +0800

    get k8s client via kubernetes/client-go

--- Golang/code/ProgrammingK8S/client-go/practice.go
+++ Golang/code/ProgrammingK8S/client-go/practice.go
@@ -1,17 +1,28 @@
 package client_go
 
 import (
+	"context"
 	"flag"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 	"log"
 )
 
-func main() {
+func GetPod() {
 	var kubeconfig = flag.String("kubeconfig", "~/.kube/config", "kubeconfig file")
 	flag.Parse()
 	config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
 	if err != nil {
-		log.Fatal("")
+		log.Fatal(err)
 	}
-	clientset, err := kubernetes.New
+	// switch protocol to protobuf from http
+	config.AcceptContentTypes = "application/vnd.kubernetes.protobuf,application/json"
+	config.ContentType = "application/vnd.kubernetes.protobuf"
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		log.Fatal(err)
+	}
+	pod, err := clientset.CoreV1().Pods("book").Get(context.Background(), "example", metav1.GetOptions{})
+	log.Printf("%+v", pod)
 }
---
+++ Golang/code/ProgrammingK8S/main.go
@@ -0,0 +1,7 @@
+package main
+
+import clientGo "github.com/hfutcbl/learn/golang/ProgrammingK8S/client-go"
+
+func main() {
+	clientGo.GetPod()
+}
