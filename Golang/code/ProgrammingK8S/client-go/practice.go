be42341be6dc5719f8f363264c3ac24f4f1003fd
i535989 <charles.cat@sap.com>
2020-10-09 16:58:54 +0800

    Add user agent in config

--- Golang/code/ProgrammingK8S/client-go/practice.go
+++ Golang/code/ProgrammingK8S/client-go/practice.go
@@ -3,10 +3,12 @@ package client_go
 import (
 	"context"
 	"flag"
+	"fmt"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/tools/clientcmd"
 	"log"
+	"runtime"
 )
 
 func GetPod() {
@@ -16,6 +18,7 @@ func GetPod() {
 	if err != nil {
 		log.Fatal(err)
 	}
+	config.UserAgent = fmt.Sprintf("book-example/v1.0 (%s/%s) kubernetes/v1.0", runtime.GOOS, runtime.GOARCH)
 	// switch protocol to protobuf from http
 	config.AcceptContentTypes = "application/vnd.kubernetes.protobuf,application/json"
 	config.ContentType = "application/vnd.kubernetes.protobuf"
